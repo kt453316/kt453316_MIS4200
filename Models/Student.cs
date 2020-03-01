00f00ba36f2913c51bae7a31e714515e5614e15a
Kelly Thaxton <kt453316@ohio.edu>
2020-03-01 14:38:01 -0500

    Getting more and more sad by the minute

--- Models/Student.cs
+++ Models/Student.cs
@@ -12,12 +12,20 @@ namespace kt453316_MIS4200.Models
         [Display(Name = "Student ID")]
         public int studentID { get; set; }
         [Display(Name = "First Name")]
+        [Required]
+        [StringLength(20)]
         public string firstName { get; set; }
         [Display(Name = "Last Name")]
+        [Required]
+        [StringLength(20)]
         public string lastName { get; set; }
         [Display(Name = "Email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string email { get; set; }
         [Display(Name = "Phone")]
+        [Required]
+        [DataType(DataType.PhoneNumber)]
         public string phone { get; set; }
 
         public ICollection<Registration> Registration { get; set; }
